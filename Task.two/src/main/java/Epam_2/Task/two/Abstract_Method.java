329221fd46891696dd5f8e256f47ad9a3ad9a5d4
91703 <91703@LAPTOP-DHG1FKQ3>
2020-02-08 23:35:39 +0530

    Final-1

---
+++ Task.two/src/main/java/Epam_2/Task/two/Abstract_Method.java
@@ -0,0 +1,6 @@
+package Epam_2.Task.two;
+public abstract class Abstract_Method {
+	public abstract int total_weight(int a[],int n);
+	public abstract int num_of_candy(int low,int high,int a[],int n);
+
+}
---
+++ Task.two/src/main/java/Epam_2/Task/two/App.java
@@ -0,0 +1,13 @@
+package Epam_2.Task.two;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
